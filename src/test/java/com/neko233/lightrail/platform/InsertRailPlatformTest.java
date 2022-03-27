fb933ee016e6783d57bb6458df8223a77c435b99
solarisneko <1417015340@qq.com>
2022-03-27 22:32:45 +0800

    1. [Add] Unit Test for Insert Template.

---
+++ src/test/java/com/neko233/lightrail/platform/InsertRailPlatformTest.java
@@ -0,0 +1,88 @@
+package com.neko233.lightrail.platform;
+
+import com.neko233.lightrail.RailPlatform;
+import com.neko233.lightrail.RailPlatformFactory;
+import com.neko233.lightrail.SqlLightRail;
+import com.neko233.lightrail.builder.InsertSqlBuilder;
+import com.neko233.lightrail.builder.SqlBuilder;
+import com.neko233.lightrail.condition.SetCondition;
+import com.neko233.lightrail.condition.WhereCondition;
+import com.neko233.lightrail.dataSource.MyDataSource;
+import com.neko233.lightrail.pojo.User;
+import com.neko233.lightrail.pojo.UserWithEmail;
+import com.sun.tools.javac.comp.Todo;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @title:
+ * @description:
+ * @author: SolarisNeko
+ * Date on: 2/26/2022
+ */
+public class InsertRailPlatformTest {
+
+    RailPlatform railPlatform = RailPlatformFactory.createLightRailPlatform(MyDataSource.getDefaultDataSource());
+
+    public InsertRailPlatformTest() throws Exception {
+    }
+
+
+    @Test
+    public void baseTest_insert2User() throws Exception {
+
+        InsertSqlBuilder builder = SqlLightRail.insertTable("user")
+            .insertColumns("name")
+            .values("('demo10'), ('demo11') ");
+        Integer rowCount = railPlatform.executeUpdate(builder);
+
+        Assert.assertTrue(2 == rowCount);
+    }
+
+    @Test
+    public void baseTest_insert2User_single_ORM() throws Exception {
+        RailPlatform railPlatform = RailPlatformFactory.createLightRailPlatform(MyDataSource.getDefaultDataSource());
+
+        List<User> valueList = new ArrayList<User>() {{
+            add(User.builder().name("demo21").build());
+            add(User.builder().name("demo22").build());
+        }};
+
+        InsertSqlBuilder builder = SqlLightRail.insertTable("user")
+            .insertColumns("name")
+            .values(valueList);
+        Integer rowCount = railPlatform.executeUpdate(builder);
+
+        Assert.assertTrue(2 == rowCount);
+    }
+
+    @Test
+    public void baseTest_insert2User_multi_ORM() throws Exception {
+        RailPlatform railPlatform = RailPlatformFactory.createLightRailPlatform(MyDataSource.getDefaultDataSource());
+
+        List<UserWithEmail> valueList = new ArrayList<UserWithEmail>() {{
+            add(UserWithEmail.builder().name("demo21").email("123@qq.com").build());
+            add(UserWithEmail.builder().name("demo22").email("456@qq.com").build());
+        }};
+
+        InsertSqlBuilder builder = SqlLightRail.insertTable("user")
+            .values(valueList);
+        Integer rowCount = railPlatform.executeUpdate(builder);
+
+        Assert.assertTrue(2 == rowCount);
+    }
+
+    @Test
+    public void generate_insert_template() {
+        String build = SqlLightRail.generateInsertTemplate(User.class, 1L).build();
+        String target = "INSERT INTO user(id, name) Values (?,?)";
+        Assert.assertEquals(target, build);
+    }
+
+
+}
